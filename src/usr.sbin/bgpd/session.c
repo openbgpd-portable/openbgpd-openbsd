964dcd12a0791b2f7bb230b9ff80442cb919ebd6
tb <>
2023-07-12 12:48:18 +0000

    Check the F_NEXTHOP flag on the right kroute6 object.
    On multipath routes the check ended up checking the wrong route for the
    nexthop update. This resulted in a use-after-free in kroute_detach_nexthop().
    This only affects IPv6 in the IPv4 code path the right object was already used.
    
    Thanks to sthen@ for providing the debug information to track this down.
    OK sthen@ tb@
    from claudio
    
    In session_process_msg() recheck the validity of the rbuf before moving
    the remaining data around.
    
    There is an improbable case where a NOTIFICATION is received while also
    reaching the MSG_PROCESS_LIMIT. In this case rbuf is NULL when breaking
    out of the for loop and hitting this code.
    
    sthen@ is the (un)lucky person to hit that improbable case
    OK tb@ sthen@
    from claudio
    
    This is errata/7.2/031_bgpd.patch.sig

--- src/usr.sbin/bgpd/session.c
+++ src/usr.sbin/bgpd/session.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: session.c,v 1.435 2022/08/31 15:51:44 claudio Exp $ */
+/*	$OpenBSD: session.c,v 1.435.2.1 2023/07/12 12:48:18 tb Exp $ */
 
 /*
  * Copyright (c) 2003, 2004, 2005 Henning Brauer <henning@openbsd.org>
@@ -1963,6 +1963,8 @@ session_process_msg(struct peer *p)
 		}
 	}
 
+	if (p->rbuf == NULL)
+		return;
 	if (rpos < av) {
 		left = av - rpos;
 		memmove(&p->rbuf->buf, p->rbuf->buf + rpos, left);
